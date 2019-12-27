5667911645cced7d060d774eea3faf5e2c038ddb
Actodi <actodi.9239@gmail.com>
2019-12-27 18:00:12 -0400

    Facultad terminada

--- app/src/main/java/com/example/mundo_lap/CarreraFragment.java
+++ app/src/main/java/com/example/mundo_lap/CarreraFragment.java
@@ -1,5 +1,6 @@
 package com.example.mundo_lap;
 
+
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
@@ -14,11 +15,19 @@ import androidx.fragment.app.FragmentTransaction;
 public class CarreraFragment extends Fragment {
 
     Button irHome;
+    Button asus;
+    Button hp;
+    Button lenovo;
+    Button acer;
+    Button mac;
+    Button dell;
+    Button toshiba;
+    Button huawei;
 
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
-        View view = inflater.inflate(R.layout.fragment_carrera,container,false);
+        View view = inflater.inflate(R.layout.fragment_facultades,container,false);
 
         irHome = view.findViewById(R.id.ir_home);
         irHome.setOnClickListener(new View.OnClickListener() {
@@ -33,6 +42,105 @@ public class CarreraFragment extends Fragment {
             }
         });
 
+        asus = view.findViewById(R.id.agronomia);
+        asus.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Fragment nuevoFragmento = new HerrOfficeFragment();
+                FragmentTransaction transaction = getFragmentManager().beginTransaction();
+                transaction.replace(R.id.fragment_container, nuevoFragmento);
+                transaction.addToBackStack(null);
+                //Commit a la transacción
+                transaction.commit();
+            }
+        });
+        hp = view.findViewById(R.id.arquitectura);
+        hp.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Fragment nuevoFragmento = new HerrOfficeFragment();
+                FragmentTransaction transaction = getFragmentManager().beginTransaction();
+                transaction.replace(R.id.fragment_container, nuevoFragmento);
+                transaction.addToBackStack(null);
+                //Commit a la transacción
+                transaction.commit();
+            }
+        });
+        lenovo = view.findViewById(R.id.derecho);
+        lenovo.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Fragment nuevoFragmento = new HerrOfficeFragment();
+                FragmentTransaction transaction = getFragmentManager().beginTransaction();
+                transaction.replace(R.id.fragment_container, nuevoFragmento);
+                transaction.addToBackStack(null);
+                //Commit a la transacción
+                transaction.commit();
+            }
+        });
+        acer = view.findViewById(R.id.economia);
+        acer.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Fragment nuevoFragmento = new HerrOfficeFragment();
+                FragmentTransaction transaction = getFragmentManager().beginTransaction();
+                transaction.replace(R.id.fragment_container, nuevoFragmento);
+                transaction.addToBackStack(null);
+                //Commit a la transacción
+                transaction.commit();
+            }
+        });
+
+        mac = view.findViewById(R.id.humanidades);
+        mac.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Fragment nuevoFragmento = new HerrOfficeFragment();
+                FragmentTransaction transaction = getFragmentManager().beginTransaction();
+                transaction.replace(R.id.fragment_container, nuevoFragmento);
+                transaction.addToBackStack(null);
+                //Commit a la transacción
+                transaction.commit();
+            }
+        });
+/*
+        dell = view.findViewById(R.id.dell);
+        dell.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Fragment nuevoFragmento = new Dell();
+                FragmentTransaction transaction = getFragmentManager().beginTransaction();
+                transaction.replace(R.id.fragment_container, nuevoFragmento);
+                transaction.addToBackStack(null);
+                //Commit a la transacción
+                transaction.commit();
+            }
+        });
+
+        toshiba = view.findViewById(R.id.toshiba);
+        toshiba.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Fragment nuevoFragmento = new Toshiba();
+                FragmentTransaction transaction = getFragmentManager().beginTransaction();
+                transaction.replace(R.id.fragment_container, nuevoFragmento);
+                transaction.addToBackStack(null);
+                //Commit a la transacción
+                transaction.commit();
+            }
+        });
+        huawei = view.findViewById(R.id.huawei);
+        huawei.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Fragment nuevoFragmento = new Huawei();
+                FragmentTransaction transaction = getFragmentManager().beginTransaction();
+                transaction.replace(R.id.fragment_container, nuevoFragmento);
+                transaction.addToBackStack(null);
+                //Commit a la transacción
+                transaction.commit();
+            }
+        });*/
         return view;
     }
 }
